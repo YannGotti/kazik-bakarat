984b06392b6f6c7620839e6504baaf1f691c5a8b
YannGotti <1111111111aleksey46@gmail.com>
2022-11-07 18:10:43 +0300

    add mysql connection string

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ BOT_TOKEN = "5708242624:AAFpfd_F13tia8AWLDGj8-2Mdous1pTnA18"
 #PAYMENTS_PROVIDER_TOKEN = "331767678:TEST:31496"
 
 ADMINS = [793228987]
-CONNECTION_STRING = 'postgresql://postgres:Yann123321AAa@localhost:5432/kazik-bakarat'
+#CONNECTION_STRING = 'postgresql://postgres:Yann123321AAa@localhost:5432/kazik-bakarat' # postgres connection
+CONNECTION_STRING = 'mysql://root:Yann123321AAa@localhost/kazik-bakarat' # mysql connection
